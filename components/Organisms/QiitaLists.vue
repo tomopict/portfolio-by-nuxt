1433909808f52c60979f6d37dfe85628c1d97497
tomopict <tomo.cat.picture@gmail.com>
2019-12-07 23:34:45 +0900

    Qiitaの情報取得をstore経由に一元化

--- components/Organisms/QiitaLists.vue
+++ components/Organisms/QiitaLists.vue
@@ -2,20 +2,11 @@
   <div>
     <ul class="flex flex-wrap -mb-4 px-4">
       <li
-        v-for="post in posts"
-        :key="post.id"
-        class="w-full sm:w-1/2 md:w-1/3 mb-6 "
-      >
-        <QiitaList :post="post" />
-      </li>
-    </ul>
-    <ul class="flex flex-wrap -mb-4 px-4">
-      <li
         v-for="post in qiitaLists"
         :key="post.id"
         class="w-full sm:w-1/2 md:w-1/3 mb-6 "
       >
-        <QiitaList :post="post" />
+        <QiitaCard :post="post" />
       </li>
     </ul>
   </div>
@@ -23,42 +14,21 @@
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
-
-import { PostLists } from '@/models/QiitaPost'
-import { fetchQiitaUserData } from '@/services/fetchQiitaData'
-
-import QiitaList from '@/components/Molecules/QiitaList.vue'
-
+import QiitaCard from '@/components/Molecules/QiitaCard.vue'
 import { qiitaModule } from '@/store/store'
 
 @Component({
   components: {
-    QiitaList
+    QiitaCard
   }
 })
 export default class QiitLists extends Vue {
-  posts: PostLists[] = []
-
   private mounted(): void {
-    this.getQiitaListsFromPage()
     this.getQiitaListsFromStore()
   }
 
   get qiitaLists(): any {
     return qiitaModule.qiitaLists
-  }
-
-  private async getQiitaListsFromPage(): Promise<void> {
-    try {
-      const res = await fetchQiitaUserData({
-        headers: {
-          Authorization: `Bearer ${process.env.Qiita}`
-        }
-      })
-      this.posts = res.data
-    } catch (e) {
-      console.log(e)
-    }
   }
 
   private async getQiitaListsFromStore(): Promise<void> {
