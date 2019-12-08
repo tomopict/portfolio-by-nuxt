8fb1ebe266b987abda59a498489c49c6b49a802c
tomopict <tomo.cat.picture@gmail.com>
2019-12-08 18:28:45 +0900

    読み込んでいないデータの削除

--- services/fetchQiitaData.ts
+++ services/fetchQiitaData.ts
@@ -1,6 +1,5 @@
 import { AxiosPromise, AxiosRequestConfig } from 'axios'
 import httpClient from './http-client'
-import { PostLists } from '~/models/QiitaPost'
 
 export const fetchQiitaUserData = (config?: AxiosRequestConfig): AxiosPromise => {
   return httpClient.get(`${process.env.QiitaApi}/users/tomopict/items`, config)
