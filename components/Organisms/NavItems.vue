bcaa2ff6964f6e5f778ed48f54be6a5a17e0b103
tomopict <tomo.cat.picture@gmail.com>
2019-12-12 22:10:14 +0900

    navitemの追加と必要最低限のページのガワだけ作成

--- components/Organisms/NavItems.vue
+++ components/Organisms/NavItems.vue
@@ -1,24 +1,30 @@
 <template>
   <div class="navitem fixed top-0 right-0">
     <nav>
-      <ul class="navitem_lists flex p-2">
-        <li class="navitem_list p-2">
-          <nuxt-link class="navitem_link p-2 block relative" to="/"
+      <ul class="navitem_lists flex">
+        <li class="navitem_list relative">
+          <nuxt-link class="navitem_link text-center p-4 block relative" to="/"
             ><span class="relative">Top</span></nuxt-link
           >
         </li>
-        <li class="navitem_list p-2">
-          <nuxt-link class="navitem_link p-2 block relative" to="/about"
+        <li class="navitem_list relative">
+          <nuxt-link
+            class="navitem_link text-center p-4 block relative"
+            to="/about"
             ><span class="relative">About</span></nuxt-link
           >
         </li>
-        <li class="navitem_list p-2">
-          <nuxt-link class="navitem_link p-2 block relative" to="/qiita"
+        <li class="navitem_list relative">
+          <nuxt-link
+            class="navitem_link text-center p-4 block relative"
+            to="/qiita"
             ><span class="relative">Qiita</span></nuxt-link
           >
         </li>
-        <li class="navitem_list p-2">
-          <nuxt-link class="navitem_link p-2 block relative" to="/blog"
+        <li class="navitem_list relative">
+          <nuxt-link
+            class="navitem_link text-center p-4 block relative"
+            to="/blog"
             ><span class="relative">Blog</span></nuxt-link
           >
         </li>
@@ -43,24 +49,52 @@ export default Vue.extend({
 </script>
 <style lang="scss" scoped>
 .navitem {
-  &_lists {
-  }
   &_list {
-    background-color: #fff;
+    color: #fff;
+    overflow: hidden;
+    width: 100px;
+    position: relative;
   }
   &_link {
     &:hover {
+      span {
+        &:after {
+          background: #1e9600; /* fallback for old browsers */
+          background: -webkit-linear-gradient(
+            to right,
+            #ff0000,
+            #fff200,
+            #1e9600
+          ); /* Chrome 10-25, Safari 5.1-6 */
+          background: linear-gradient(
+            to right,
+            #ff0000,
+            #fff200,
+            #1e9600
+          ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+          opacity: 1;
+          width: 70px;
+        }
+        &:before {
+          width: 70px;
+        }
+      }
     }
     span {
-      &:after {
+      &:after,
+      &:before {
+        transition: all 0.25s ease-in;
         content: '';
         display: inline-block;
-        width: 100%;
+        width: 30px;
         height: 1px;
-        background: #000;
+        background: #efefef;
         bottom: -5px;
         left: 0;
         position: absolute;
+      }
+      &:after {
+        opacity: 0;
       }
     }
   }
