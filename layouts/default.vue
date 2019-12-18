e896e74bce5bb083b6832e1078e938bc78171375
tomopict <tomo.cat.picture@gmail.com>
2019-12-18 22:58:23 +0900

    topでやっていた処理をレイアウトに移動

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,81 @@
 <template>
-  <div class=" bg-gray-400 w-screen h-full lg:p-20">
+  <div class="w-screen h-full lg:p-20" :class="`timeslot-${timeSlot}`">
     <nav-items />
     <nuxt />
   </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
+import { reactive, computed } from '@vue/composition-api'
 import NavItems from '@/components/Organisms/NavItems.vue'
+
+import { fetchWeatherData } from '@/services/fetchWeatherData'
+import usePromise from '@/composables/use-promise.ts'
+
+interface weatherParamModels {
+  p: string
+  APPID: string
+}
 
 export default Vue.extend({
   name: 'Default',
   components: {
     NavItems
+  },
+  setup(props: {}, context) {
+    const timeSlot = computed(() => {
+      const time = +context.root.$dayjs().format('HH')
+      switch (true) {
+        case time < 5:
+          return 'night'
+        case time < 8:
+          return 'earlymorning'
+        case time < 12:
+          return 'morinig'
+        case time < 17:
+          return 'evening'
+        case time < 19:
+          return 'sunset'
+        default:
+          return 'night'
+      }
+    })
+
+    return {
+      timeSlot
+    }
+  },
+  asyncData() {
+    const weatherParam = reactive({
+      q: 'tokyo',
+      APPID: `${process.env.WEATHER}`
+    })
+    const getEvents = usePromise(weatherParam => fetchWeatherData(weatherParam))
+    getEvents.createPromise(weatherParam)
   }
 })
 </script>
 
-<style>
+<style lang="scss" scoped>
+.timeslot {
+  &-earlymorning {
+    background-color: #f9c5d1;
+    background-image: linear-gradient(315deg, #f9c5d1 0%, #9795ef 74%);
+  }
+  &-morning,
+  &-evening {
+    background-color: #7aa7f9;
+    background-image: linear-gradient(315deg, #7aa7f9 0%, #577ff1 74%);
+  }
+  &-sunset {
+    background-color: #a40606;
+    background-image: linear-gradient(315deg, #a40606 0%, #d98324 74%);
+  }
+  &-night {
+    background-color: #2a305a;
+    background-image: linear-gradient(316deg, #2a305a 0%, #141050 74%);
+  }
+}
 .page-enter-active,
 .page-leave-active {
   transition: opacity 0.3s;
