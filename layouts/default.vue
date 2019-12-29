0e7450ca5612ecde862076502d7e7d90c23b9499
tomopict <tomo.cat.picture@gmail.com>
2019-12-30 08:08:00 +0900

    layoutsから余計な処理を除去

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,11 @@
 <template>
-  <div class="w-screen h-full lg:p-20" :class="`timeslot-${timeSlot}`">
-    <nav-items />
+  <div class="w-screen lg:p-20 text-gray-700">
     <nuxt />
   </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
-import { reactive, computed } from '@vue/composition-api'
-import NavItems from '@/components/Organisms/NavItems.vue'
+import { reactive, computed, SetupContext } from '@vue/composition-api'
 
 import { fetchWeatherData } from '@/services/fetchWeatherData'
 import usePromise from '@/composables/use-promise.ts'
@@ -19,32 +17,6 @@ interface weatherParamModels {
 
 export default Vue.extend({
   name: 'Default',
-  components: {
-    NavItems
-  },
-  setup(props: {}, context) {
-    const timeSlot = computed(() => {
-      const time = +context.root.$dayjs().format('HH')
-      switch (true) {
-        case time < 5:
-          return 'night'
-        case time < 8:
-          return 'earlymorning'
-        case time < 12:
-          return 'morinig'
-        case time < 17:
-          return 'evening'
-        case time < 19:
-          return 'sunset'
-        default:
-          return 'night'
-      }
-    })
-
-    return {
-      timeSlot
-    }
-  },
   asyncData() {
     const weatherParam = reactive({
       q: 'tokyo',
@@ -57,25 +29,6 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
-.timeslot {
-  &-earlymorning {
-    background-color: #f9c5d1;
-    background-image: linear-gradient(315deg, #f9c5d1 0%, #9795ef 74%);
-  }
-  &-morning,
-  &-evening {
-    background-color: #7aa7f9;
-    background-image: linear-gradient(315deg, #7aa7f9 0%, #577ff1 74%);
-  }
-  &-sunset {
-    background-color: #a40606;
-    background-image: linear-gradient(315deg, #a40606 0%, #d98324 74%);
-  }
-  &-night {
-    background-color: #2a305a;
-    background-image: linear-gradient(316deg, #2a305a 0%, #141050 74%);
-  }
-}
 .page-enter-active,
 .page-leave-active {
   transition: opacity 0.3s;
