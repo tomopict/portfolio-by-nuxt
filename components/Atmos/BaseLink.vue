0b9ec51c350ef2bc2ccb9d4fc3fec35263fbe131
tomopict <tomo.cat.picture@gmail.com>
2019-12-30 08:03:57 +0900

    linkのbasecomponentを追加

---
+++ components/Atmos/BaseLink.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <nuxt-link class="text-white" :to="link">
+      <slot></slot>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    link: {
+      type: String,
+      default: '',
+      required: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+a {
+  text-decoration: underline;
+}
+</style>
