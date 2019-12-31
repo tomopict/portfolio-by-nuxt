9bbe1b60e3ffcbcadef6cefb43dbda8ff8569e81
tomopict <tomo.cat.picture@gmail.com>
2019-12-31 09:50:44 +0900

    Qiita関連ページの調整

--- components/Organisms/QiitaLists.vue
+++ components/Organisms/QiitaLists.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <ul class="flex flex-wrap -mb-4 px-4 bg-white">
+    <ul class="flex flex-wrap">
       <li
-        v-for="post in qiitaLists"
+        v-for="post in qiitaLists.slice(0, limit)"
         :key="post.id"
-        class="w-full sm:w-1/2 md:w-1/3 mb-6 "
+        class="w-full sm:w-1/2 md:w-1/3 mb-2"
       >
         <QiitaCard :post="post" />
       </li>
@@ -13,7 +13,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator'
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
 import QiitaCard from '@/components/Molecules/QiitaCard.vue'
 import { qiitaModule } from '@/store/store'
 
@@ -26,6 +26,7 @@ export default class QiitLists extends Vue {
   private mounted(): void {
     this.getQiitaListsFromStore()
   }
+  @Prop({ type: Number, required: false }) limit!: 0
 
   get qiitaLists(): any {
     return qiitaModule.qiitaLists
--- pages/qiita.vue
+++ pages/qiita.vue
@@ -1,25 +1,24 @@
 <template>
-  <div>
-    <!-- <BaseInput
-      v-model="state.message"
-      label="Description"
-      type="text"
-      placeholder="Description"
-      class="field"
-      @change-message="changeMessage"
-    /> -->
+  <div class="p-2 bg-gray-200">
+    <h2 class="font-bold text-center mb-6 m-auto text-2xl">Qiita</h2>
     <QiitaLists />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator'
+import Vue from 'vue'
 import QiitaLists from '@/components/Organisms/QiitaLists.vue'
 
-@Component({
+export default Vue.extend({
+  name: 'Qiita',
   components: {
     QiitaLists
   }
 })
-export default class Qiita extends Vue {}
 </script>
+<style lang="scss" scoped>
+h2 {
+  border-bottom: 1px solid #4a5568;
+  max-width: 200px;
+}
+</style>
