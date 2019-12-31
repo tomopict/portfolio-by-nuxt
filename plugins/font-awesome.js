68f35d41359305b89add4069b4a14be2a78463bb
tomopict <tomo.cat.picture@gmail.com>
2019-12-31 09:51:59 +0900

    font-awwesomeで使用するアイコンを修正

--- plugins/font-awesome.js
+++ plugins/font-awesome.js
@@ -2,15 +2,11 @@ import Vue from 'vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faPlus, faStar } from '@fortawesome/free-solid-svg-icons'
-import {
-  faTwitter,
-  faInstagram,
-  faLine
-} from '@fortawesome/free-brands-svg-icons'
+import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(faPlus, faStar)
-library.add(faTwitter, faInstagram, faLine)
+library.add(faTwitter, faGithub)
 
 Vue.component('fa-icon', FontAwesomeIcon)
 
