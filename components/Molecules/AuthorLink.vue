72e77e00248980a04de4092715a258c011b89830
tomopict <tomo.cat.picture@gmail.com>
2020-01-04 18:52:04 +0900

    twitterなどのデータを外だし

---
+++ components/Molecules/AuthorLink.vue
@@ -0,0 +1,25 @@
+<template>
+  <a :href="linkdata.link" target="_blank" class="block">
+    <base-icon
+      :icotype="'fab'"
+      :name="linkdata.value"
+      :size="'lg'"
+      :style="{ color: 'white' }"
+    />
+  </a>
+</template>
+
+<script lang="ts">
+import { createComponent } from '@vue/composition-api'
+
+export default createComponent({
+  name: 'AuthorLink',
+  props: {
+    linkdata: {
+      type: Object,
+      required: true
+    }
+  }
+})
+</script>
+<style lang="scss" scoped></style>
