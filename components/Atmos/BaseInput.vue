8b22b80d817d0bf56444dfd0b6b95260b6364876
tomopict <tomo.cat.picture@gmail.com>
2020-01-04 18:54:49 +0900

    APIの名称を変更

---
+++ components/Atmos/BaseInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <input :value="value" v-bind="$attrs" @input="updateValue" />
+  </div>
+</template>
+
+<script lang="ts">
+import { SetupContext } from '@vue/composition-api'
+export default {
+  inheritAttrs: false,
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    value: [String, Number]
+  },
+  setup(props, context: SetupContext) {
+    const updateValue = event => {
+      context.emit('change-message', event.target.value)
+    }
+    return {
+      updateValue
+    }
+  }
+}
+</script>
