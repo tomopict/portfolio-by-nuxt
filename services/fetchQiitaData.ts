d3c5e3913fb78c83961b8c1dadbbb518422ae0f2
tomopict <tomo.cat.picture@gmail.com>
2020-03-01 08:05:32 +0900

    reafact

--- services/fetchQiitaData.ts
+++ services/fetchQiitaData.ts
@@ -1,6 +1,8 @@
 import { AxiosPromise, AxiosRequestConfig } from 'axios'
 import httpClient from './http-client'
 
-export const fetchQiitaUserData = (config?: AxiosRequestConfig): AxiosPromise => {
+export const fetchQiitaUserData = (
+  config?: AxiosRequestConfig
+): AxiosPromise => {
   return httpClient.get(`${process.env.QiitaApi}/users/tomopict/items`, config)
 }
--- services/fetchWeatherData.ts
+++ services/fetchWeatherData.ts
@@ -1,11 +1,11 @@
 import { AxiosPromise, AxiosRequestConfig } from 'axios'
 import httpClient from './http-client'
 
-export const fetchWeatherData = (
-  parms: {
-    q: string,
-    APPID: string
-  }
-): AxiosPromise => {
-  return httpClient.get(`http://api.openweathermap.org/data/2.5/weather?q=${parms.q}&APPID=${parms.APPID}`)
+export const fetchWeatherData = (parms: {
+  q: string
+  APPID: string
+}): AxiosPromise => {
+  return httpClient.get(
+    `http://api.openweathermap.org/data/2.5/weather?q=${parms.q}&APPID=${parms.APPID}`
+  )
 }
